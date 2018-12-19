36e9b413032f5047abca46a745dc4f0ac5adb9c7
yashwanthsg <45753542+yashwanthsg@users.noreply.github.com>
2018-12-19 22:37:28 +0530

    Add files via upload

---
+++ DefaultValue.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class DefaultValue {
+boolean x;
+int y;
+char z;
+String a;
+float b;
+double c;
+public static void main(String[] args) {
+	DefaultValue obj=new DefaultValue();
+	 System.out.println(obj.x);
+	 System.out.println(obj.y);
+	 System.out.println(obj.z);
+	 System.out.println(obj.a);
+	 System.out.println(obj.b);
+	 System.out.println(obj.c);
+}
+}
